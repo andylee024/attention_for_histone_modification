f60ed1a800832b951b2c9f710b27ea0379f07fec
Andy Lee <andylee024@gmail.com>
2017-10-19 23:46:59 -0400

    Change config and mock_data to satisfy general attention problem

--- libs/attention_model/mock_data.py
+++ libs/attention_model/mock_data.py
@@ -2,32 +2,18 @@ import collections
 import numpy as np
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Public APIs for creating mock data
+# Mock data structures for ML
 # ----------------------------------------------------------------------------------------------------------------------
 
+TrainingExample = collections.namedtuple(
+    typename='TrainingExample', field_names=['sequence', 'annotation', 'label'])
 
-def create_dummy_training_example(attention_config):
-    """Create a single training example with dummy data according to attention configuration object."""
+TrainingTensor = collections.namedtuple(
+    typename="TrainingTensor", field_names=['sequence_tensor', 'annotation_tensor', 'label_tensor'])
 
-    annotation_vector_size = (attention_config.number_of_annotation_vectors,
-                              attention_config.annotation_vector_dimension)
-
-    dummy_sequence = np.random.randint(low=0,
-                                       high=attention_config.vocabulary_size,
-                                       size=attention_config.sequence_length)
-
-    dummy_sequence_one_hot = convert_to_one_hot(labels=dummy_sequence,
-                                                number_of_classes=attention_config.vocabulary_size)
-
-    dummy_label = np.random.randint(low=0,
-                                    high=attention_config.prediction_classes,
-                                    size=1)
-
-    dummy_annotation_vectors = np.random.normal(loc=0.0, scale=1.0, size=annotation_vector_size)
-
-    return TrainingExample(sequence=dummy_sequence_one_hot,
-                           annotation_vectors=dummy_annotation_vectors,
-                           label=dummy_label)
+# ----------------------------------------------------------------------------------------------------------------------
+# Public APIs for creating mock data
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 def create_dummy_batch_data(attention_config):
@@ -36,18 +22,46 @@ def create_dummy_batch_data(attention_config):
     ;return:
         Tensor representation of training examples.
     """
-    training_examples = [create_dummy_training_example(attention_config) for _ in xrange(attention_config.batch_size)]
+    training_examples = [create_dummy_training_example(
+        attention_config) for _ in xrange(attention_config.batch_size)]
     return convert_training_examples_to_tensor(training_examples)
 
-# ----------------------------------------------------------------------------------------------------------------------
-# Mock data structures for ML
-# ----------------------------------------------------------------------------------------------------------------------
 
-TrainingExample = collections.namedtuple(
-    typename='TrainingExample', field_names=['sequence', 'annotation_vectors', 'label'])
+def create_dummy_training_example(attention_config):
+    """Create a single training example with dummy data according to attention configuration object."""
+    return TrainingExample(sequence=create_dummy_training_sequence(attention_config, one_hot=True),
+                           label=create_dummy_training_label(
+                               attention_config, one_hot=True),
+                           annotation=create_dummy_training_annotation(attention_config))
 
-TrainingTensor = collections.namedtuple(
-    typename="TrainingTensor", field_names=['sequence_tensor', 'annotation_tensor', 'label_tensor'])
+
+def create_dummy_training_sequence(attention_config, one_hot=True):
+    """Create dummy training sequence from attention config."""
+    dummy_sequence = np.random.randint(low=0,
+                                       high=attention_config.vocabulary_size,
+                                       size=attention_config.sequence_length)
+    if one_hot:
+        dummy_sequence = convert_to_one_hot(labels=dummy_sequence,
+                                            number_of_classes=attention_config.vocabulary_size)
+    return dummy_sequence
+
+
+def create_dummy_training_label(attention_config, one_hot=True):
+    """Create dummy training label from attention config."""
+    dummy_label = np.random.randint(
+        low=0, high=attention_config.prediction_classes, size=1)
+    if one_hot:
+        dummy_label = convert_to_one_hot(
+            labels=dummy_label,
+            number_of_classes=attention_config.prediction_classes)
+    return dummy_label
+
+
+def create_dummy_training_annotation(attention_config):
+    """Create dummy training annotation from attention config."""
+    return np.random.normal(loc=0.0,
+                            scale=1.0,
+                            size=(attention_config.number_of_annotations, attention_config.annotation_size))
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Mock data
@@ -73,7 +87,8 @@ def convert_to_one_hot(labels, number_of_classes):
     @param number_of_classes: number of label classes
     @return: one-hot encoding of labels (N x C), where N is batch size, C is number of classes
     """
-    one_hot_labels = [convert_label_to_one_hot(l, number_of_classes=number_of_classes) for l in labels]
+    one_hot_labels = [convert_label_to_one_hot(
+        l, number_of_classes=number_of_classes) for l in labels]
     return np.concatenate(one_hot_labels, axis=0)
 
 
@@ -89,11 +104,12 @@ def convert_training_examples_to_tensor(training_examples):
     # Add batch dimension to tensors and concatenate matrices across batch dimension
     # np.expand_dims - adds batch dimension
     # np.concatenate - creates batch tensor by stacking on batch dimension
-    sequence_tensor = np.concatenate([np.expand_dims(te.sequence, axis=0) for te in training_examples], axis=0)
-    annotation_tensor = np.concatenate([np.expand_dims(te.annotation_vectors, axis=0) for te in training_examples],
-                                       axis=0)
-    label_tensor = np.concatenate([np.expand_dims(te.label, axis=0) for te in training_examples], axis=0)
-    label_tensor = np.ravel(label_tensor)
+    sequence_tensor = np.concatenate(
+        [np.expand_dims(te.sequence, axis=0) for te in training_examples], axis=0)
+    annotation_tensor = np.concatenate(
+        [np.expand_dims(te.annotation, axis=0) for te in training_examples], axis=0)
+    label_tensor = np.concatenate(
+        [te.label for te in training_examples], axis=0)
 
     return TrainingTensor(sequence_tensor=sequence_tensor,
                           annotation_tensor=annotation_tensor,
