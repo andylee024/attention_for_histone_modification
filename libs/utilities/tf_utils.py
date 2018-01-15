c63a317be4e9a6fad481b8ce307b9833e08f572a
Andy <andylee024@gmail.com>
2018-01-15 13:39:57 -0500

    Add model loading functionality

--- libs/utilities/tf_utils.py
+++ libs/utilities/tf_utils.py
@@ -13,3 +13,24 @@ def load_inference_graph_into_session(trained_model_directory, sess):
                                tags=[tf.saved_model.tag_constants.SERVING],
                                export_dir=trained_model_directory)
     return tf.get_default_graph()
+
+
+def load_checkpoint_into_session(model_checkpoint, sess):
+    """Load saved model graph into session.
+
+    :param model_checkpoint: tensorflow model_checkpoint file
+    :param sess: tensorflow session
+    :return: tensorflow graph containing recovered ops
+    """
+    saver = tf.train.import_meta_graph(_convert_checkpoint_to_meta_graph_file(model_checkpoint))
+    saver.restore(sess, model_checkpoint)
+    return tf.get_default_graph()
+
+
+def _convert_checkpoint_to_meta_graph_file(model_checkpoint):
+    """Convert model checkpoint to corresponding meta graph file.
+    
+    :param model_checkpoint: tensorflow model_checkpoint file
+    :return: metagraph file
+    """
+    return model_checkpoint + ".meta"
